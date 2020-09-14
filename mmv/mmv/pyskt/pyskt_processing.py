c88e19c2ea43b2204e7c0b20bb97667680bc25bd
Tremeschin <tremeschin@protonmail.com>
2020-09-14 16:14:55 -0400

    Get info of point relative to any irregular polygon

--- mmv/mmv/pyskt/pyskt_processing.py
+++ mmv/mmv/pyskt/pyskt_processing.py
@@ -26,6 +26,29 @@ import skia
 class PysktProcessing:
     def __init__(self, pyskt_main):
         self.pyskt_main = pyskt_main
+    
+    # # #
+
+    # https://stackoverflow.com/questions/7261936
+
+    # https://stackoverflow.com/a/63013204/13477696
+    def num2col(self, n):
+        n, rem = divmod(n - 1, 26)
+        next_char = chr(65 + rem)
+        if n:
+            return self.num2col(n) + next_char
+        else:
+            return next_char
+    
+    # https://stackoverflow.com/a/63013204/13477696
+    def col2num(self, s):
+        n = ord(s[-1]) - 64
+        if s[:-1]:
+            return 26 * (self.col2num(s[:-1])) + n
+        else:
+            return n
+            
+    # # # 
 
     # Absolute area between two vectors so doesn't matter what direction they are at
     def abs_cross_product(self, V1, V2):
@@ -53,10 +76,109 @@ class PysktProcessing:
         # for a triangle we divide that by two
         return self.abs_cross_product(V1, V2) / 2
 
+    # https://stackoverflow.com/a/24468019/13477696
+    def irregular_polygon_area(self, *points):
+        n = len(points) # of corners
+        area = 0.0
+        for i in range(n):
+            j = (i + 1) % n
+            area += points[i][0] * points[j][1]
+            area -= points[j][0] * points[i][1]
+        area = abs(area) / 2.0
+        return area
+
+    # Information of a point relative to a polygon
+    def information_point_polygon(self, P, *polygon_points):
+        """
+        Given a point P and a polygon of N points, returns:
+        {
+            "is_inside": bool,
+            "minimum_distance": float,
+            "minimum_distance_line": see below
+        }
+
+        The minimum distance line is something like, you give this function
+        (P=(x, y), (0, 0), (10, 5), (3, 5)) =>
+        (P, A, B, C)
+
+        If point nearest line is AB, minimum_distance_line returns 0.
+
+        AB - 0   CB - 1   CA - 2
+        """
+
+        # Get 
+        polygon_area = self.irregular_polygon_area(*polygon_points)
+
+        P = np.array(P)
+
+        # Ordered points: (0, 0), (1, 0), (0, 1)  -->  [('A', (0, 0)), ('B', (1, 0)), ('C', (0, 1))]
+        # We use list as those will be easier to manipulate
+        op = [(self.num2col(i + 1), np.array(v)) for i, v in enumerate(polygon_points)]
+
+        # [('A', (0, 0)), ('B', (1, 0)), ('C', (0, 1))] Becomes:
+        # {'CAP': {'points': [array([0, 1]), array([0, 0]), array([1, 1])], 'area': 0.5}, 'ABP': {'points': ...
+        triangles = {f"{op[i][0]}-{op[i+1][0]}-P": {"points": [op[i][1], op[i+1][1], P]} for i in range(-1, len(op) - 1)}
+        
+        # Now we convert to a dictionary for matching the points later on
+        op = {point: coordinate for point, coordinate in op}
+
+        # Calculate 
+        for key in triangles.keys():
+            triangles[key]["area"] = self.three_points_triangle_area(*triangles[key]["points"])
+
+        print(triangles)
+
+        # Sum the triangle areas
+        triangles_areas = [triangles[key]["area"] for key in triangles.keys()]
+
+        # The point is inside the polygon if the areas sum less than the original one
+        is_inside = sum(triangles_areas) <= polygon_area
+
+        # Get the triangle with the lowest area so we'll find the closest edge
+        lowest_area = min(triangles_areas)
+        for key in triangles.keys():
+            if triangles[key]["area"] == lowest_area:
+                smallest_triangle = key.split("-")
+                break
+
+        # To get our minimum_distance_line
+        # If the point is AB, we return 0
+        # If the point is BC, we return 1
+        # If the point is CA, we return 2
+        # It is the lowest index of the two points
+        point_indexes = [
+            self.col2num(smallest_triangle[0]),
+            self.col2num(smallest_triangle[1]),
+        ]
+        print(point_indexes, smallest_triangle)
+        minimum_distance_line = min(point_indexes) - 1
+        
+        # Now we have a triangle in the form XX-YY-P, we'll find the height of the triangle, the minimum distance from P to 
+        # the line of XX -- YY
+
+        # The triangle base is the distance between the points (XX) and (YY) so..
+        base = np.linalg.norm(
+            [ op[smallest_triangle[0]], op[smallest_triangle[1]] ]
+        )
+        
+        # We know its area, it's the lowest_area var!!
+        # B*H = A,
+        # H = A/B
+        lowest_distance = lowest_area / base
+
+        info = {
+            "is_inside": is_inside,
+            "minimum_distance": lowest_distance,
+            "minimum_distance_line": minimum_distance_line,
+        }
+
+        print(triangles_areas, polygon_area, is_inside, "smallest:", smallest_triangle)
+        print(info)
+
     # Is a point of coordinate P = (x, y) inside a rectangle R = (x, y, w, h)?
     # https://math.stackexchange.com/a/190117
     # This was a pretty fun insight :)
-    def point_inside_rectangle(self, P, R, method="dot_product"):
+    def is_point_inside_rectangle(self, P, R, method="dot_product"):
         """
         R1 - - - - - - R2
          |     P       |
@@ -82,37 +204,7 @@ class PysktProcessing:
         R3 = np.array([x, y - h])
         R4 = np.array([x + w, y - h])
 
-        # Far more efficient
-        if method == "dot_product":
-            R1R2 = R2 - R1
-            R1R3 = R3 - R1
-            R1P = P - R1
-            return (0 <= np.dot(R1P, R1R2) < np.dot(R1R2, R1R2)) and (0 <= np.dot(R1P, R1R3) < np.dot(R1R3, R1R3))
-
-        # Nice insight on areas
-        elif method == "triangles":
-
-            # # Rectangle
-
-            # We'll get the vector from center R1 and goes to R2 and R3
-            # V1 = R1R2 => R2 - R1
-            # V2 = R1R3 => R3 - R1
-            rectangle_area = self.abs_cross_product(R2 - R1, R3 - R1)
-
-            # # Triangle
-
-            # Triangles points -> (R1 R3 P), (R1, R2, P), (R2, R4, P), (R4, R3, P)
-            triangle_areas = [
-                self.three_points_triangle_area(R1, R3, P),
-                self.three_points_triangle_area(R1, R2, P),
-                self.three_points_triangle_area(R2, R4, P),
-                self.three_points_triangle_area(R4, R3, P),
-            ]
-
-            if sum(triangle_areas) <= rectangle_area:
-                return True
-            else:
-                return False
+        return self.information_point_polygon(R1, R2, R4, R3)
 
 if __name__ == "__main__":
     
@@ -120,9 +212,10 @@ if __name__ == "__main__":
 
     p = PysktProcessing({})
 
-    print( p.three_points_triangle_area([0, 0], [0, 3], [4, 0]) ) 
+    p.information_point_polygon((1, 0.1), (0, 0), (1, 0), (0, 1))
+    # print( p.three_points_triangle_area([0, 0], [0, 3], [4, 0]) ) 
 
-    print(p.point_inside_rectangle(
-        [1, 0],
-        [0, 0, 10, 10]
-    ))
+    # print(p.is_point_inside_rectangle(
+    #     [1, 0],
+    #     [0, 0, 10, 10]
+    # ))
